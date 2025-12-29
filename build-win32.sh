77915906164130a721529ecc03af4fec4dc169ec
meowpoof89 <73567894+meowpoof89@users.noreply.github.com>
2025-12-29 17:11:36 -0500

    Rename Jirenyte to DalorianScape in build script

--- build-win32.sh
+++ build-win32.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo Launcher sha256sum
-sha256sum build/libs/Jirenyte.jar
+sha256sum build/libs/DalorianScape.jar
 
 cmake -S liblauncher -B liblauncher/build32 -A Win32
 cmake --build liblauncher/build32 --config Release
@@ -24,8 +24,8 @@ fi
 
 echo "$WIN32_CHKSUM win32_jre.zip" | sha256sum -c
 
-cp native/build-x86/src/Release/Jirenyte.exe build/win-x86/
-cp build/libs/Jirenyte.jar build/win-x86/
+cp native/build-x86/src/Release/DalorianScape.exe build/win-x86/
+cp build/libs/DalorianScape.jar build/win-x86/
 cp packr/win-x86-config.json build/win-x86/config.json
 cp liblauncher/build32/Release/launcher_x86.dll build/win-x86/
 
@@ -33,9 +33,9 @@ unzip win32_jre.zip
 mv jdk-$WIN32_VERSION-jre build/win-x86/jre
 
 echo Jirenyte.exe 32bit sha256sum
-sha256sum build/win-x86/Jirenyte.exe
+sha256sum build/win-x86/DalorianScape.exe
 
-dumpbin //HEADERS build/win-x86/Jirenyte.exe
+dumpbin //HEADERS build/win-x86/DalorianScape.exe
 
 # We use the filtered iss file
 iscc build/filtered-resources/runelite32.iss
